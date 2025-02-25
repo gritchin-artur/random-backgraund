f8d25e25b7d63c1a809721b1d6383d026b5eb130
artur <grit4in0225@icloud.com>
2025-02-25 10:06:39 +0100

    deleted comment

--- src/index.js
+++ src/index.js
@@ -24,13 +24,13 @@ button.textContent = 'click \n #565454';
 button.addEventListener('click', () => {
     let randomColor = '#';
     for (let i = 0; i < 6; i++) {
-        const randomIndex = Math.floor(Math.random() * characterArray.length); // variant 1
+        const randomIndex = Math.floor(Math.random() * characterArray.length); 
         randomColor += characterArray[randomIndex];
     }
 
     let randomColorBtn = `#${Math.floor(Math.random() * 16777215)
         .toString(16)
-        .padStart(6, '0')}`; // variant 2
+        .padStart(6, '0')}`; 
     button.style.background = randomColorBtn;
     buttonContainer.style.background = randomColor;
     button.textContent = `click \n ${randomColor}`;
