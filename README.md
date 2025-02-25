f12666c3cd65f14df0337b60d7c6be06960f2d83
artur <grit4in0225@icloud.com>
2025-02-25 10:03:21 +0100

    done project

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Simple calculator
+# Random Background Color
--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log('Hello, world!');
+const buttonContainer = document.getElementById('button-container');
+const button = document.getElementById('button');
+
+const characterArray = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F'
+];
+
+button.textContent = 'click \n #565454';
+button.addEventListener('click', () => {
+    let randomColor = '#';
+    for (let i = 0; i < 6; i++) {
+        const randomIndex = Math.floor(Math.random() * characterArray.length); // variant 1
+        randomColor += characterArray[randomIndex];
+    }
+
+    let randomColorBtn = `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, '0')}`; // variant 2
+    button.style.background = randomColorBtn;
+    buttonContainer.style.background = randomColor;
+    button.textContent = `click \n ${randomColor}`;
+});
